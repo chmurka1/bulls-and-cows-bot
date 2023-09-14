a558b61855421fcd5fae399107d215fac1c5395d
Dominik Chmura <dompawchmura@gmail.com>
2023-09-15 01:13:33 +0200

    Add bulls and cows implementation

---
+++ bullsncows/src/game.rs
@@ -0,0 +1,49 @@
+use rand::{seq::IteratorRandom, thread_rng};
+
+pub struct Game {
+    guess_count : u64,
+    answer : String
+}
+
+impl Game {
+    pub fn new(length : usize) -> Game {
+        let mut rng = thread_rng();
+        let sample = (0..9).choose_multiple(&mut rng, length);
+
+        let answer = sample.iter().map(|u| -> char {
+                char::from_digit(*u, 10).expect("Gamee error")
+            }).collect();
+
+        return Game { guess_count: 0, answer };
+    }
+
+    pub fn get_answer(&self) -> &str {
+        return self.answer.as_str();
+    }
+
+    pub fn get_guess_count(&self) -> u64 {
+        return self.guess_count;
+    }
+
+    pub fn get_answer_length(&self) -> usize {
+        self.answer.len()
+    }
+
+    pub fn make_guess(&mut self, guess: &str) -> Option<(usize, usize)> {
+        if guess.len() != self.answer.len() || !guess.chars().all(char::is_alphanumeric) {
+            return None;
+        }
+
+        self.guess_count += 1;
+        
+        let bulls = guess.chars().zip(self.answer.chars()).filter(|(a, b)| {
+            a == b
+        }).count();
+
+        let cows = guess.chars().filter(|&c| {
+            self.answer.contains(c)
+        }).count();
+
+        return Some((bulls, cows - bulls));
+    }
+}
---
+++ bullsncows/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+mod game;
+
+fn main() {
+    let mut g = game::Game::new(4); 
+    println!("Gues the number: {}", g.get_answer());
+    
+    for _ in 1..100 {
+        let mut user_guess = String::new();
+        std::io::stdin().read_line(&mut user_guess).expect("Error while reading user input");
+        user_guess = String::from(user_guess.trim());
+
+        match g.make_guess(&user_guess) {
+            None => {
+                println!("Invalid input!");
+                continue;
+            }
+            Some((bulls, cows)) => {
+                println!("Bulls: {}, cows: {}", bulls, cows);
+
+                if bulls == g.get_answer_length() && cows == 0 {
+                    println!("You won after {} guesses", g.get_guess_count())
+                }
+            }
+        }
+    }
+}
